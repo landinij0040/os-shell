778e3460e7920eac81b56891922a1048777a83ac
landinij0040 <ijlandin0040@miners.utep.edu>
2021-02-04 14:59:16 -0700

    git init

---
+++ shell.py
@@ -0,0 +1,18 @@
+import os
+# Use only
+ # pipe()
+ # fork()
+ # dup() or dup2()
+ # execve()
+ # wait()
+ # open() or create() and close()
+ # read() and write()
+ # chdir()
+import sys
+import re
+
+if __name__ == "__main__":
+    os.chdir(os.getcwd())
+    # while(1){
+    #     input = os.read(fdIn, 10000)
+    # }
